fe45a5cf7e0f0473ef399cd8a29cfde230164df5
Alaina Kneiling <alaina.noel.8@gmail.com>
2023-01-13 15:43:32 -0700

    search bar visbile

--- src/App.js
+++ src/App.js
@@ -1,22 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {useState} from "react";
+import SearchForm from './components/SearchForm/SearchForm';
 
-function App() {
+const App = () =>  {
+  const [games, setGames] = useState([]);
+  const queryGames = (searchValue) => {
+    const endpoint = `https://api.scorebooklive.com/v2/games?date=2022-09-13&priority_order=true`;
+    fetch(endpoint)
+      .then(response => response.json())
+      .then(data => setGames(data.data))
+      // .catch(error => {
+      //   // Handle any errors
+      // }); //TODO handle this error
+  }
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {/* <Header /> */}
+        <div className="body">
+        <SearchForm queryGames={queryGames} />
+        {/* <Games games={games} /> */}
+        </div>
       </header>
     </div>
   );
---
+++ src/components/SearchForm/SearchForm.js
@@ -0,0 +1,40 @@
+import React, { useState } from "react";
+import './SearchForm.css';
+
+const SearchForm = ({ queryGames }) => {
+  const [searchValue, setSearchValue] = useState("");
+  const [domError, setDomError] = useState("");
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    if (searchValue) {
+      queryGames(searchValue);
+      clearInputs();
+    } else {
+      setDomError("Please enter a date");
+    }
+  };
+
+  const clearInputs = () => { 
+    setSearchValue("");
+    setDomError("");
+  };
+
+  return (
+    <div className="search-area">
+      <h3>Find A Game by Date: CHange this later</h3>
+      <input
+        type="text"
+        placeholder="Search for a List of Games by Date"
+        name="gme"
+        className="search-input"
+        value={searchValue}
+        onChange={(e) => setSearchValue(e.target.value)}
+      />
+      <button className="game-search-btn" onClick={(event) => handleSubmit(event)}>Search</button>
+      {domError && <h4 className="error-message">{domError}</h4>}
+    </div>
+  );
+};
+
+export default SearchForm;
