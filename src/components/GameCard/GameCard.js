cf8a1fb0fdd3b7841fb7d0959a6121468a314408
Alaina Kneiling <alaina.noel.8@gmail.com>
2023-01-14 09:52:00 -0700

    contents of card decided and written

---
+++ src/components/GameCard/GameCard.js
@@ -0,0 +1,21 @@
+import React from 'react'
+// import {Link} from 'react-router-dom' //TODO: add a link to team info
+import './GameCard.css'
+
+export const GameCard = ({ awayPhoto, homePhoto, awayMascotName, homeMascotName, gameStatus, sport }) => {
+    return(
+        <div className='game-card'>
+            <div className='game-card-title'>
+                <p>{homeMascotName} at {awayMascotName}</p>
+                <p> Status: {gameStatus} </p>
+                <p> Sport: {sport} </p>
+            </div>
+            <div className='teamLogos'>
+            <img className='away-team-image' src={awayPhoto} alt='away-team-logo'/>
+            <img className='home-team-image' src={homePhoto} alt='home-team-logo'/>
+            </div>
+        </div>
+    )
+}
+
+export default GameCard;
